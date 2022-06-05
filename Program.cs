c144a7a0e38533aba77244a762e39d853ccfec08
Alina Podvalnaia <alinapo1997@gmail.com>
2022-06-05 21:06:34 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace PW5
+{
+    internal class Program
+    {
+        #region Split Words [Метод разделения строки на слова]
+        static void Main(string[] args)        {   
+
+            Console.WriteLine("Введите предложение, которое необходимо разделить:");
+            string line = Console.ReadLine();
+            var arrayWords = SplitSentence(line);
+            ShowArray(arrayWords);
+            MixWords();
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Метод разделяющий предложение на слова по пробелу 
+        /// </summary>
+        /// <param name="line">Разделяемая строка</param>
+        /// <returns>Возвращает массив из слов</returns>
+        static string[] SplitSentence(string line)
+        {
+            return line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+        /// <summary>
+        /// Метод выводящий на экран массив из строк
+        /// </summary>
+        /// <param name="stringArray">Массив строк</param>
+        static void ShowArray(string[] stringArray)
+        {
+            foreach (var word in stringArray)
+            {
+                Console.WriteLine(word);
+            }
+        }
+        #endregion
+
+        #region Mix Words [Перестановка слов в предложении]
+        static void MixWords()
+        {
+            Console.WriteLine("Введите предложение, в котором будет перемешан порядок слов:");
+            string line = Console.ReadLine();
+            Console.WriteLine($"Ваше предложение в обратном порядке -> {ReverseWords(line)}");
+        }
+        static string ReverseWords(string inputPhrase)
+        {
+            var arrayPhrases = SplitSentence(inputPhrase);
+            string result = "";
+            for(int i = arrayPhrases.Length - 1; i >= 0; i--)
+            {
+                result += $"{arrayPhrases[i]} ";                
+            }
+            return result;
+        }
+        
+        #endregion
+    }
+}
